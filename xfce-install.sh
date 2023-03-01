33892501afe674e2417e8cce3c4e6523219da912
Erne D <112326674+erned98@users.noreply.github.com>
2023-03-01 10:05:31 +0100

    Update xfce-install.sh

--- xfce-install.sh
+++ xfce-install.sh
@@ -2,29 +2,32 @@
 
 ## configure and install minimal xfce desktop environment
 
+## check for sudo/root
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must run with sudo, try again..."
+  exit 1
+fi
 
-sudo cat ./xsessionrc >> /home/$SUDO_USER/.xsessionrc
-sudo chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.xsessionrc
+cat ./xsessionrc >> /home/$SUDO_USER/.xsessionrc
+chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.xsessionrc
 
-sudo apt install -y \
-     libxfce4ui-utils \
-     thunar \
-     xfce4-appfinder \
-     xfce4-panel \
-     xfce4-pulseaudio-plugin \
-     xfce4-whiskermenu-plugin \
-     xfce4-session \
-     xfce4-settings \
-     xfce4-terminal \
-     xfconf \
-     xfdesktop4 \
-     xfwm4 \
-     qt5ct 
+apt install -y \
+    libxfce4ui-utils \
+    thunar \
+    xfce4-appfinder \
+    xfce4-panel \
+    xfce4-pulseaudio-plugin \
+    xfce4-whiskermenu-plugin \
+    xfce4-session \
+    xfce4-settings \
+    xfce4-terminal \
+    xfconf \
+    xfdesktop4 \
+    xfwm4 \
+    qt5ct 
     
-## additional theming, in case you don't want it, just comment it
-git clone https://github.com/erned98/xfce4-dots && cd xfce4-dots && chmod +x setup && ./setup
 
 echo 
-echo xfce install complete, please reboot and issue 'startx'
+echo "xfce install complete, for additional theming, run 'theming.sh' as a user, then reboot and issue 'startx'"
 echo
 
