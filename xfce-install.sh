c58fbc4d468ca84dc441445d69ab8a524e2be9bd
Erne D <112326674+erned98@users.noreply.github.com>
2023-03-01 09:47:42 +0100

    Update xfce-install.sh

--- xfce-install.sh
+++ xfce-install.sh
@@ -2,31 +2,26 @@
 
 ## configure and install minimal xfce desktop environment
 
-## check for sudo/root
-if ! [ $(id -u) = 0 ]; then
-  echo "This script must run with sudo, try again..."
-  exit 1
-fi
 
-cat ./xsessionrc >> /home/$SUDO_USER/.xsessionrc
-chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.xsessionrc
+sudo cat ./xsessionrc >> /home/$SUDO_USER/.xsessionrc
+sudo chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.xsessionrc
 
-apt install -y \
-    libxfce4ui-utils \
-    thunar \
-    xfce4-appfinder \
-    xfce4-panel \
-    xfce4-pulseaudio-plugin \
-    xfce4-whiskermenu-plugin \
-    xfce4-session \
-    xfce4-settings \
-    xfce4-terminal \
-    xfconf \
-    xfdesktop4 \
-    xfwm4 \
-    qt5ct 
+sudo apt install -y \
+     libxfce4ui-utils \
+     thunar \
+     xfce4-appfinder \
+     xfce4-panel \
+     xfce4-pulseaudio-plugin \
+     xfce4-whiskermenu-plugin \
+     xfce4-session \
+     xfce4-settings \
+     xfce4-terminal \
+     xfconf \
+     xfdesktop4 \
+     xfwm4 \
+     qt5ct 
     
-# additional theming, in case you don't want it, just comment it
+## additional theming, in case you don't want it, just comment it
 git clone https://github.com/erned98/xfce4-dots && cd xfce4-dots && chmod +x setup && ./setup
 
 echo 
